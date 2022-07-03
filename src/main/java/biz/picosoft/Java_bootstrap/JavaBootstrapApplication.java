d2224371bcc247bd6f3b41a4cc9c7db1d4755988
emna_kamoun <kamounemna94@gmail.com>
2022-07-03 19:17:10 +0100

    first commit

---
+++ src/main/java/biz/picosoft/Java_bootstrap/JavaBootstrapApplication.java
@@ -0,0 +1,13 @@
+package biz.picosoft.Java_bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBootstrapApplication.class, args);
+	}
+
+}
