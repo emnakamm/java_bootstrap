b360f9dbbfd3b8ce77c065bf04625becab402000
emna_kamoun <kamounemna94@gmail.com>
2022-08-07 13:08:08 +0100

    config

--- src/main/java/biz/picosoft/Java_bootstrap/JavaBootstrapApplication.java
+++ src/main/java/biz/picosoft/Java_bootstrap/JavaBootstrapApplication.java
@@ -1,9 +1,12 @@
 package biz.picosoft.Java_bootstrap;
+ 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "biz.picosoft.javabootstrap.controllers")
 public class JavaBootstrapApplication {
 
 	public static void main(String[] args) {
